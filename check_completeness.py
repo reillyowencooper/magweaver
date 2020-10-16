115d0a03021322c6ac39ad0147f2416e63ff8e93
reillyowencooper <reilly.cooper@huskers.unl.edu>
2020-10-15 21:48:50 -0500

    Wrote completeness checking script, should output CSV file of CheckM results to specified folder

--- check_completeness.py
+++ check_completeness.py
@@ -1,1 +1,30 @@
-# TODO: Write command line script for completeness.py
+import argparse
+from src.completeness import CompletenessChecker
+
+def parse_args():
+    parser = argparse.ArgumentParser(description = "Quantify completion and contamination of MAGs using CheckM")
+    required = parser.add_argument_group('Required arguments')
+    optional = parser.add_argument_group('Optional arguments')
+    required.add_argument('--mag_dir', help = 'Directory containing your input MAGs')
+    required.add_argument('--output_dir', help = 'Directory to output summary')
+    optional.add_argument('--completeness', help = 'Minimum completeness to qualify as High Quality, default 95')
+    optional.add_argument('--contamination', help = 'Maximum contamination to qualify as High Quality, default 5')
+    args = parser.parse_args()
+    return args
+
+
+def main():
+    args = parse_args()
+    completor = CompletenessChecker(args.mag_dir,
+                                       args.output_dir,
+                                       args.completeness,
+                                       args.contamination)
+    completor.create_logger()
+    completor.create_output_dir()
+    completor.run_checkm()
+    contig_count = completor.count_contigs()
+    final_df = completor.summarize_completeness_contam(contig_count)
+    completor.df_to_csv(final_df)
+
+if __name__ == "__main__":
+    main()
